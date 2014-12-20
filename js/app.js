a95342b2f86360dd7ac9a683cce93946da2e3e7f
Shaun Kelly <shaunkelly@gmail.com>
2014-12-20 14:06:46 -0500

    Add basic animate code for enemy

--- js/app.js
+++ js/app.js
@@ -6,8 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
-    this.y = 0;
+    this.velocity = 100;
+    this.x = -101;
+    this.y = 60; // row 1 = 60; row 2 = 145; row 3 = 
 }
 
 // Update the enemy's position, required method for game
@@ -16,6 +17,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x < ctx.canvas.width) {
+      this.x = this.x + (dt * this.velocity);
+    }
 }
 
 // Draw the enemy on the screen, required method for game
